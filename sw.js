1c05a6fc2d572479b46e533f646ff2feb8a6fee0
Senu14 <101559338+Senu14@users.noreply.github.com>
2023-04-18 08:47:04 +0200

    App and aw.js files crated

---
+++ sw.js
@@ -0,0 +1,8 @@
+// Install Service Worker
+self.addEventListener('install', event => {
+	console.log('Service Worker has been installed');
+})
+// Install Service Worker
+self.addEventListener('activate', event => {
+	console.log('Service Worker has been activated');
+})
